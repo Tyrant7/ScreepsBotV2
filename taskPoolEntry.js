b8b71325ba293432d4438603bcdf0e4c3e707afc
Tyrant <tyler.sauve@bag-of-holding.com>
2023-12-17 11:26:01 -0800

    Docstring updates

--- taskPoolEntry.js
+++ taskPoolEntry.js
@@ -3,12 +3,22 @@ const distanceWeight = 1;
 
 class TaskPoolEntry {
 
+    /**
+     * Creates a new TaskPoolEntry object.
+     * @param {number} priority The base priority of this entry.
+     * @param {Task} task The task associated with this entry. 
+     */
     constructor(priority, task) {
         this.basePriority = priority;
         this.age = 0;
         this.task = task;
     }
 
+    /**
+     * Calculates the priority of this entry.
+     * @param {RoomPosition} position The position to use for distance calculations.
+     * @returns The weighted priority for this entry.
+     */
     getPriority(position) {
         return this.basePriority + 
                 (this.age * ageWeight) + 
