886810c728e8d875d45e9137394ce5a180cf0674
Tyrant <tyler.sauve@bag-of-holding.com>
2024-01-16 20:55:15 -0800

    Fixed incorrect docstring

--- task.js
+++ task.js
@@ -3,7 +3,7 @@ class Task {
     /**
      * Creates a new task with the following properties:
      * @param {string} target The object ID of the target. Can be a resource, a structure, a construction site; anything with a position.
-     * @param {number} tag A tag to distinguish this task when filtering task types.
+     * @param {string} tag A tag to distinguish this task when filtering task types.
      * @param {function[]} actionStack An array of functions to be executed while completing this task. 
      * Functions will be called in order through the action stack until one returns false.
      * @param {number} priority The priority of this task. Higher priority tasks will be chosen over lower priority tasks. Default of 0.
