52b7aebf633713e23e8c7bce7e962ae187c7ce74
Tyrant <tyler.sauve@bag-of-holding.com>
2024-01-28 15:59:34 -0800

    Responsibility list for remote manager

--- remoteManager.js
+++ remoteManager.js
@@ -7,6 +7,36 @@ class RemoteManager {
 
     run(roomInfo, remainingSpawnCapacity) {
         
+
+        // Responsiblity list:
+        //
+        // 1. Planning remotes
+        // -> remote planner module will handle this part
+        //
+        // 2. Tracking remote efficiency and printing out an overlay in the base room
+        // -> printing remote name, gross energy production, net energy production, 
+        //    upkeep, spawn cost, and current creep vs ideal creep counts
+        //
+        // 3. Tracking states for each remote
+        // -> constructing: remote is in progress of being built
+        //    - requesting builders
+        //    - handling placement of construction sites in a streamlined manner
+        //    - ensuring that only one remote per base room is being constructed at a time for efficiency
+        // -> active: remote is healthy and producing
+        //    - handling maintenance if roads become too low or destroyed
+        //    - requesting additional haulers if containers overflow
+        //    - handle searching for threats in this remote or nearby
+        // -> contested: remote is under potential threat
+        //    - requesting defenders
+        //    - handling of setting certain flags for present miners and haulers to be aware 
+        //      and react appropriately by fleeing
+        //    - if contest goes on for too long or cost is too great abandon the remote
+        // -> ruined: remote has been abandoned
+        //    - should handle flagging the room as dangerous and keeping track of the threat
+        //    - should also calculate if best course of action is to retake the room or to 
+        //      attempt to build a new remote somewhere else once threat has subsided for other rooms
+
+
         this.temporary(roomInfo, remainingSpawnCapacity);
     }
 
