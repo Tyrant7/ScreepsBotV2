2d621cb70bf052d8da65ba745ec816c7ead38081
Tyrant <tyler.sauve@bag-of-holding.com>
2024-02-24 11:19:09 -0800

    Better rolling average data

--- trackCPU.js
+++ trackCPU.js
@@ -1,13 +1,13 @@
-const periodDuration = 15;
-
 module.exports = function() {
     if (!Memory.stats) {
         Memory.stats = {};
     }
     if (!Memory.stats.rollingAverage) {
-        Memory.stats.rollingAverage = Game.cpu.getUsed();
+        Memory.stats.rollingAverage = 0;
+        Memory.stats.nSamples = 0;
     }
     const used = Game.cpu.getUsed();
-    Memory.stats.rollingAverage = ((Memory.stats.rollingAverage * (periodDuration - 1)) + used) / periodDuration;
+    Memory.stats.nSamples++;
+    Memory.stats.rollingAverage = ((Memory.stats.rollingAverage * (Memory.stats.nSamples - 1)) + used) / Memory.stats.nSamples;
     return Memory.stats.rollingAverage;
 }
