bd156b73db701f70ea63fe2dc55df060b954f951
Tyrant <tyler.sauve@bag-of-holding.com>
2024-03-19 21:20:12 -0700

    A bit more progress. Will leave the rest for tomorrow

--- spawnHandlerUsage.js
+++ spawnHandlerUsage.js
@@ -1,5 +1,110 @@
-class UsageSpawnHandler {
+const remoteUtility = require("remoteUtility");
+const creepSpawnUtility = require("creepSpawnUtility");
+const creepMaker = require("creepMakerUsage");
 
+class UsageSpawnHandler {
+    
+    getNextSpawn(roomInfo) {
+
+        // Get our best spawn
+        const spawn = this.getFirstSpawn(roomInfo, this.getExistingSpawns(roomInfo));
+        if (spawn) {
+            return spawn;
+        }
+
+        // Otherwise, no spawns needed
+        return null;
+    }
+
+    getFirstSpawn(roomInfo, existingSpawns) {
+
+        // TODO //
+        // TEMPLATE CODE; TAKEN FROM OTHER SCRIPT AND NOT YET FUNCTIONAL //
+        // PLEASE REPLACE //
+
+        function checkIfSpawn() {
+            if (wantedMiners > existingSpawns.miners) {
+                return creepMaker.makeMiner(roomInfo.room.energyCapacityAvailable);
+            }
+            if (wantedReservers > existingSpawns.reservers) {
+                return creepMaker.makeReserver();
+            }
+            
+            // Since haulers are handled by part count and level, let's first check to see if this
+            // room is requesting more or as many haulers as we already have
+            // If it's more, we can add another of appropriate level
+            // If it's the same, we'll add another if we don't have it yet, effectively restructuring our hauler configuration
+            // e.x. 8C + 4C; 4C dies -> add a 12C; 8C dies -> do nothing since we now have our ideal level configuration
+            if (wantedCarry >= existingSpawns.haulerCarry) {
+                const idealHaulerLevels = this.getIdealLevels(Math.ceil(wantedCarry / 2), 
+                    CONSTANTS.maxHaulerLevel, 
+                    creepMaker.haulerLevelCost, 
+                    roomInfo.room.energyCapacityAvailable);
+                const missingHaulerLevel = this.getMissingLevel(idealHaulerLevels, existingSpawns.haulers);
+                if (missingHaulerLevel) {
+                    return creepMaker.makeHauler();
+                }
+            }
+        }
+
+        // Main room first
+        let wantedMiners = roomInfo.getSources().length;
+        let wantedReservers = 0;
+        let wantedCarry = roomInfo.getMaxIncome();
+        const nextSpawn = checkIfSpawn();
+        if (nextSpawn) {
+            return nextSpawn;
+        }
+
+        // Iterate over each remote until we find one that hasn't had its need met yet
+        const remotes = remoteUtility.getRemotePlans(roomInfo.room.name);
+        for (const remoteRoom in remotes) {
+            const remote = remotes[remoteRoom];
+            if (!remote.active) {
+                continue;
+            }
+
+            wantedMiners += remote.sourceCount;
+            wantedCarry += remote.neededCarry;
+            wantedReservers += 1;
+
+            // If we're missing anything, let's get to spawning
+            const nextSpawn = checkIfSpawn();
+            if (nextSpawn) {
+                return nextSpawn;
+            }
+        }
+    }
+
+    /**
+     * Determines the existing spawn counts for creeps owned by this room.
+     * @param {RoomInfo} roomInfo The info object for the room to count spawns for.
+     * @returns {{}} An object mapping roles to either creep count or an array existing levels, depending on creep type.
+     */
+    getExistingSpawns(roomInfo) {
+
+        // Track our existing spawns
+        // Same deal as tracking ideal spawns, but this time for creeps that already exist
+        const existingSpawns = {
+            miners: 0,
+            reservers: 0,
+            haulerCarry: 0,
+            haulers: [],
+        };
+
+        existingSpawns.miners = roomInfo.miners.length;
+        existingSpawns.reservers = roomInfo.reservers.length;
+
+        // We'll have to do something slightly different for haulers that are measured by part counts
+        haulerCarry = roomInfo.haulers.reduce((total, hauler) => {
+            return total + hauler.body.filter((p) => p.type === CARRY);
+        }, 0);
+        existingSpawns.haulers = this.getLevels(roomInfo.haulers, function(hauler) {
+            return hauler.body.filter((p) => p.type === MOVE).length;
+        });
+
+        return existingSpawns;
+    }
 }
 
 module.exports = UsageSpawnHandler;
