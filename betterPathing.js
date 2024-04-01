2e2b083dca58272800a7f84a004dd3f6313bb1ce
Tyrant <tyler.sauve@bag-of-holding.com>
2024-04-01 09:47:44 -0700

    Pathing utility class

--- betterPathing.js
+++ betterPathing.js
@@ -1,3 +1,4 @@
+const utility = require("betterPathingUtility");
 const profiler = require("profiler");
 
 let moveRegistry = {};
@@ -51,10 +52,10 @@ Creep.prototype.betterMoveTo = function(target, options) {
                 // Raise maxOps and try again
                 continue;
             }
-            return serializePath(result.path);
+            return utility.serializePath(result.path);
         }
         // console.log("No path could be found from " + startPos + " to " + goals.pos + " with range " + goals.range + ". Using incomplete path!");
-        return serializePath(result.path);
+        return utility.serializePath(result.path);
     }
 
     function verifyPath(creep) {
@@ -77,9 +78,9 @@ Creep.prototype.betterMoveTo = function(target, options) {
         }
 
         // If we moved last time, we should be right on our path
-        const nextStep = getNextStep(moveData.path, creep.pos);
+        const nextStep = utility.getNextStep(moveData.path, creep.pos);
         if (creep.pos.isEqualTo(nextStep)) {
-            return progressPath(moveData.path, creep.pos);
+            return utility.progressPath(moveData.path, creep.pos);
         }
 
         // Something went wrong with our pathing
@@ -97,7 +98,7 @@ Creep.prototype.betterMoveTo = function(target, options) {
 
     const path = verifyPath(this);
     if (path.length) {
-        const nextStep = getNextStep(path, this.pos);
+        const nextStep = utility.getNextStep(path, this.pos);
         const direction = this.pos.getDirectionTo(nextStep); 
         if (direction) {
             drawArrow(this.pos, direction, { color: "#00FF00" });
@@ -123,7 +124,7 @@ Creep.prototype.move = function(direction) {
     this.wrappedMove(direction);
 
     // If there's a creep standing where we want to go, let's request a shove
-    this.shoveIfNecessary(getPosInDirection(this.pos, direction));
+    this.shoveIfNecessary(utility.getPosInDirection(this.pos, direction));
 }
 Creep.prototype.shoveIfNecessary = function(targetPos) {
     if (!targetPos) {
@@ -206,30 +207,13 @@ function drawArrow(pos, direction, style) {
     if (!DEBUG.drawOverlay || !DEBUG.drawTrafficArrows) {
         return;
     }
-    const target = getPosInDirection(pos, direction);
+    const target = utility.getPosInDirection(pos, direction);
     if (!target) {
         return;
     }
     const x = target.x - ((target.x - pos.x) * 0.5);
     const y = target.y - ((target.y - pos.y) * 0.5);
     Game.rooms[pos.roomName].visual.line(pos.x, pos.y, x, y, style);
-}
-
-// Utility function
-function getPosInDirection(startPos, direction) {
-    const directions = {
-        [TOP]:          [0, -1],
-        [TOP_RIGHT]:    [1, -1],
-        [RIGHT]:        [1,  0],
-        [BOTTOM_RIGHT]: [1,  1],
-        [BOTTOM]:       [0,  1],
-        [BOTTOM_LEFT]:  [-1, 1],
-        [LEFT]:         [-1, 0],
-        [TOP_LEFT]:     [-1,-1],
-    }
-    const newX = (startPos.x + directions[direction][0]) % 50;
-    const newY = (startPos.y + directions[direction][1]) % 50;
-    return { x: newX, y: newY };
 }
 
 let cachedCostMatrices = {};
@@ -259,75 +243,4 @@ function getCachedCostMatrix(roomName) {
 
     cachedCostMatrices[roomName] = { tick: Game.time, costs: matrix };
     return matrix;
-}
-
-/**
- * Serializes a path as a starting position and array of directions into a single string.
- * @param {RoomPosition[]} path The path to serialize.
- * @param {boolean} endPathIfNoVisibility A flag telling us to end the path once we enter a new room with no visibility.
- * Disable this setting if we're using a cached path. If we're pathing normally we'll usually want this enabled to prevent us
- * from accidentally pathing into unwalkable built structures that we had no vision on when we started pathing.
- * @returns {string} The path in serialized form.
- */
-function serializePath(path, endPathIfNoVisibility) {
-    let serializedPath = "";
-    if (!path[0]) {
-        return serializedPath;
-    }
-
-    // Serialize our starting position
-    serializedPath += path[0].x < 10 ? "0" + path[0].x : path[0].x.toString();
-    serializedPath += path[0].y < 10 ? "0" + path[0].y : path[0].y.toString();
-
-    // Create an array of directions to follow from here
-    let lastPos = path[0];
-    for (const pos of path) {
-        if (pos === lastPos) {
-            continue;
-        }
-        serializedPath += lastPos.getDirectionTo(pos);
-        lastPos = pos;
-        if (endPathIfNoVisibility && !Game.rooms[pos.roomName]) {
-            break;
-        }
-    }
-    return serializedPath;
-}
-
-/**
- * Gets the next step in the path. 
- * @param {string} serializedPath The serialized path to step through.
- * @param {RoomPosition} currentPos The position of the creep following the path. Used for determining the next room. 
- * @returns {RoomPosition} The next position to move to in the path.
- */
-function getNextStep(serializedPath, currentPos) {
-    const nextX = parseInt(serializedPath.substring(0, 2));
-    const nextY = parseInt(serializedPath.substring(2, 4));
-    return new RoomPosition(nextX, nextY, currentPos.roomName);
-}
-
-/**
- * Advances the path by one step, and returns it.
- * @param {string} serializedPath The path to advance.
- * @param {RoomPosition} currentPos The position of the creep following the path. Used for determining the next room. 
- * @returns {string} The serialized path, progressed one step.
- */
-function progressPath(serializedPath, currentPos) {
-    // First identify our next position
-    const lastPos = getNextStep(serializedPath, currentPos);
-
-    // Then cut our next step out of our path
-    serializedPath = serializedPath.substring(4);
-    if (!serializedPath.length) {
-        return "";
-    }
-
-    // Finally, append our next position to our path
-    const nextPos = getPosInDirection(lastPos, serializedPath[0]);
-    const nextX = nextPos.x < 10 ? "0" + nextPos.x : nextPos.x.toString();
-    const nextY = nextPos.y < 10 ? "0" + nextPos.y : nextPos.y.toString();
-    const prefix = nextX + nextY;
-
-    // return our new position and directions
-    return prefix + serializedPath.substring(1);
 }
