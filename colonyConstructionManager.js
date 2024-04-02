977857c12fbc8f3768132888dd20ccfc052a8ee3
Tyrant <tyler.sauve@bag-of-holding.com>
2024-04-02 15:58:36 -0700

    Improvements to upgrader container planning

--- colonyConstructionManager.js
+++ colonyConstructionManager.js
@@ -19,38 +19,20 @@ class ColonyConstructionManager {
         let pos = base.upgraderContainer;
         if (!pos) {
             // Generate a placement position
-            // By looking for the most accessible position within range 3 of the controller
+            // By looking for the most accessible position within range 2 of the controller
 
-            // Get all positions within range of 3 from the controller
-            const positions = [];
-            const controllerPos = roomInfo.room.controller.pos;
-            for (let x = -3; x < 3; x++) {
-                for (let y = -3; y < 3; y++) {
-                    const realX = controllerPos.x + x;
-                    const realY = controllerPos.y + y;
-                    if (realX <= 0 || realX >= 49 || realY <= 0 || realY >= 49) {
-                        continue;
-                    }
-                    positions.push(roomInfo.room.getPositionAt(realX, realY));
-                }
-            }
+            // Get all positions within range of 2 from the controller
+            const positions = this.getAdjacentPositions(roomInfo.room.controller.pos, 2);
 
-            // Find the valid position adjacent to the most roads that isn't a road itself
-            const bestPos = positions.reduce((best, curr) => {
-                if (!best) {
-                    return curr;
-                }
-                const invalid = roomInfo.room.getTerrain(curr.x, curr.y) === TERRAIN_MASK_WALL;
-                const hasRoad = roomInfo.room.lookForAt(LOOK_STRUCTURES, curr.x, curr.y).find((s) => s.structureType === STRUCTURE_ROAD);
-                if (invalid || hasRoad) {
-                    return best;
-                }
-                const currRoads = roomInfo.room.lookForAtArea(LOOK_STRUCTURES, curr.y-1, curr.x-1, curr.y+1, curr.x+1, true).filter(
-                    (s) => s.structure.structureType === STRUCTURE_ROAD).length;
-                const bestRoads = roomInfo.room.lookForAtArea(LOOK_STRUCTURES, best.y-1, best.x-1, best.y+1, best.x+1, true).filter(
-                    (s) => s.structure.structureType === STRUCTURE_ROAD).length;
-                return currRoads > bestRoads ? curr : best;
-            }, null);
+            // Find the closest valid position that has the most open positinos around it
+            const terrain = roomInfo.room.getTerrain();
+            const bestPos = positions.filter((position) => {
+                return terrain.get(position.x, position.y) !== TERRAIN_MASK_WALL;
+            }).reduce((best, curr) => {
+                const currOpenSpaces = this.getAdjacentPositions(curr).filter((p) => terrain.get(p.x, p.y) !== TERRAIN_MASK_WALL).length;
+                const bestOpenSpaces = this.getAdjacentPositions(best).filter((p) => terrain.get(p.x, p.y) !== TERRAIN_MASK_WALL).length;
+                return currOpenSpaces > bestOpenSpaces ? curr : best;
+            }, positions[0]);
 
             bestPos.createConstructionSite(STRUCTURE_CONTAINER);
             Memory.bases[roomInfo.room.name].upgraderContainer = bestPos;
@@ -88,23 +70,8 @@ class ColonyConstructionManager {
         // Find the position adjacent to the target that's also adjacent to the most walls,
         // while still being walkable in order to keep miners out of the way
 
-        function getAdjacentPositions(pos) {
-            const positions = [];
-            for (let x = -1; x <= 1; x++) {
-                for (let y = -1; y <= 1; y++) {
-                    const realX = pos.x + x;
-                    const realY = pos.y + y;
-                    if (realX <= 0 || realX >= 49 || realY <= 0 || realY >= 49) {
-                        continue;
-                    }
-                    positions.push(new RoomPosition(realX, realY, pos.roomName));
-                }
-            }
-            return positions;
-        }
-
         // Get all adjacent positions to the source
-        const adjacent = getAdjacentPositions(forPos);
+        const adjacent = this.getAdjacentPositions(forPos);
 
         // If we already have a container here, let's exit early, no need to plan
         for (const pos of adjacent) {
@@ -129,12 +96,12 @@ class ColonyConstructionManager {
             }
 
             // Count all adjacent walls to the current position
-            const currAdjacent = getAdjacentPositions(curr).reduce((total, p) => {
+            const currAdjacent = this.getAdjacentPositions(curr).reduce((total, p) => {
                 total + (terrain.get(p.x, p.y) === TERRAIN_MASK_WALL ? 1 : 0);
             }, 0);
 
             // Count all adjacent walls to the best position
-            const bestAdjacent = getAdjacentPositions(best).reduce((total, p) => {
+            const bestAdjacent = this.getAdjacentPositions(best).reduce((total, p) => {
                 total + (terrain.get(p.x, p.y) === TERRAIN_MASK_WALL ? 1 : 0);
             }, 0);
 
@@ -148,6 +115,21 @@ class ColonyConstructionManager {
         // Let's record the container in memory and create the construction site
         bestPosition.createConstructionSite(STRUCTURE_CONTAINER);
         return bestPosition;
+    }
+
+    getAdjacentPositions(pos, range = 1) {
+        const positions = [];
+        for (let x = -range; x <= range; x++) {
+            for (let y = -range; y <= range; y++) {
+                const realX = pos.x + x;
+                const realY = pos.y + y;
+                if (realX <= 0 || realX >= 49 || realY <= 0 || realY >= 49) {
+                    continue;
+                }
+                positions.push(new RoomPosition(realX, realY, pos.roomName));
+            }
+        }
+        return positions;
     }
 }
 
