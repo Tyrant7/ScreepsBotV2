ceecfe966ad70a0923fec046c426df7f9abcbbfc
Tyrant <tyler.sauve@bag-of-holding.com>
2024-05-18 10:38:49 -0700

    Prettier format on main.js and renamed base planner

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ global.DEBUG = {
     trackCreepCounts: true,
     trackActiveRemotes: true,
     trackSpawnUsage: true,
-    
+
     logRemotePlanning: true,
     logRemoteDropping: true,
     replanRemotesOnReload: false,
@@ -81,7 +81,7 @@ const creepRoleMap = {
 const economyManager = new EconomyManager();
 
 // Base planning
-const BasePlanner = require("./basePlanner");
+const BasePlanner = require("./base.basePlanner");
 const basePlanner = new BasePlanner();
 
 // Defense
@@ -99,8 +99,7 @@ const overlay = require("./overlay");
 const trackStats = require("./trackStats");
 const profiler = require("./profiler");
 
-module.exports.loop = function() {
-
+module.exports.loop = function () {
     // Passive pixel generation
     // Disabled on most servers
     if (Game.cpu.generatePixel) {
@@ -108,7 +107,7 @@ module.exports.loop = function() {
             Game.cpu.generatePixel();
         }
     }
-    
+
     // Initialize our colonies
     for (const room in Game.rooms) {
         if (!Game.rooms[room].controller || !Game.rooms[room].controller.my) {
@@ -137,10 +136,18 @@ module.exports.loop = function() {
         if (DEBUG.trackRCLProgress) {
             overlay.addHeading(info.room.name, "- RCL -");
             const averageRCL = trackStats.trackRCL(info.room.name);
-            overlay.addText(info.room.name, { "RCL Per Tick": averageRCL.toFixed(3) });
-            const neededEnergyToNextRCL = info.room.controller.progressTotal - info.room.controller.progress;
-            const ticksUntilNextRCL = Math.floor(neededEnergyToNextRCL / averageRCL);
-            overlay.addText(info.room.name, { "Next RCL In": ticksUntilNextRCL});
+            overlay.addText(info.room.name, {
+                "RCL Per Tick": averageRCL.toFixed(3),
+            });
+            const neededEnergyToNextRCL =
+                info.room.controller.progressTotal -
+                info.room.controller.progress;
+            const ticksUntilNextRCL = Math.floor(
+                neededEnergyToNextRCL / averageRCL
+            );
+            overlay.addText(info.room.name, {
+                "Next RCL In": ticksUntilNextRCL,
+            });
         }
 
         // Defense
@@ -148,7 +155,6 @@ module.exports.loop = function() {
 
         // Hauling requests
         haulingRequester.generateBasicRequests(info);
-
 
         // DEBUG
         basePlanner.run(info);
@@ -159,7 +165,6 @@ module.exports.loop = function() {
     for (const name in Memory.creeps) {
         const creep = Game.creeps[name];
         if (creep) {
-
             // Skip haulers until the end
             if (creep.memory.role === CONSTANTS.roles.hauler) {
                 continue;
@@ -168,18 +173,19 @@ module.exports.loop = function() {
             // Map the creep's role to its appropriate manager and run behaviour
             if (creepRoleMap[creep.memory.role]) {
                 profiler.startSample(creep.name);
-                creepRoleMap[creep.memory.role].processCreep(creep, roomInfos[creep.memory.home]);
+                creepRoleMap[creep.memory.role].processCreep(
+                    creep,
+                    roomInfos[creep.memory.home]
+                );
                 profiler.endSample(creep.name);
-            }
-            else {
+            } else {
                 creep.say("Missing");
             }
-        }
-        else {
+        } else {
             creepDeath(name);
         }
     }
-    // We'll process all haulers after ordinary creeps, in case other creeps created orders this tick 
+    // We'll process all haulers after ordinary creeps, in case other creeps created orders this tick
     for (const info of Object.values(roomInfos)) {
         for (const hauler of info.haulers) {
             profiler.startSample(hauler.name);
@@ -189,13 +195,13 @@ module.exports.loop = function() {
     }
     profiler.endSample("Creeps");
 
-    // Track CPU usage 
+    // Track CPU usage
     // (don't track reload because it leads to innacurate averages which take a long time to equalize)
     if (DEBUG.trackCPUUsage && !RELOAD) {
         const rollingAverage = trackStats.trackCPU();
         for (const info of Object.values(roomInfos)) {
             overlay.addHeading(info.room.name, "- CPU Usage -");
-            overlay.addText(info.room.name, { 
+            overlay.addText(info.room.name, {
                 "Average CPU": rollingAverage.toFixed(3),
                 "Last CPU": Game.cpu.getUsed().toFixed(3),
             });
@@ -207,7 +213,7 @@ module.exports.loop = function() {
         for (const info of Object.values(roomInfos)) {
             overlay.addHeading(info.room.name, "- Creeps -");
             overlay.addText(info.room.name, {
-                "Count": Object.values(Memory.creeps).length,
+                Count: Object.values(Memory.creeps).length,
             });
         }
     }
@@ -221,14 +227,13 @@ module.exports.loop = function() {
 
     // If we reloaded
     global.RELOAD = false;
-}
+};
 
 /**
  * Processes the death of a creep to run any cleanup code.
  * @param {string} name The name of the deceased creep.
  */
 function creepDeath(name) {
-
     const role = Memory.creeps[name].role;
     if (creepRoleMap[role]) {
         creepRoleMap[role].freeCreep(name);
