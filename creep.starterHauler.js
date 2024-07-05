c1406d3eafa0f6ca5147b8e99efee9f83a477ea5
Tyrant <tyler.sauve@bag-of-holding.com>
2024-07-05 13:15:16 -0700

    Fixed another bug with baby haulers

--- creep.starterHauler.js
+++ creep.starterHauler.js
@@ -4,7 +4,7 @@ const { roles } = require("./constants");
 class StarterHaulerManager extends HaulerManager {
     createTask(creep, roomInfo) {
         // Once we have another hauler up and running, we'll turn this hauler into a scout
-        if (roomInfo.haulers.length > 1) {
+        if (roomInfo.haulers.length) {
             creep.memory.role = roles.scout;
             return null;
         }
