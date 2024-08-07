150b9dc19a9a41423296651ddcec855231712766
Tyrant <tyler.sauve@bag-of-holding.com>
2024-08-07 12:38:32 -0700

    Typo fix

--- combat.missionCommands.js
+++ combat.missionCommands.js
@@ -1,7 +1,7 @@
 const { setHate } = require("./combat.missionUtility");
 
 const registerMissionCommands = () => {
-    global.SET_HATE = (player, hate) => {
+    global.SET_HATE = (player, amount) => {
         setHate(player, amount);
     };
 };
