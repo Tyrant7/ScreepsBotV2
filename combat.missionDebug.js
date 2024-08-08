69945d5f976276668040c05b36e393a193bebb71
Tyrant <tyler.sauve@bag-of-holding.com>
2024-08-07 21:55:29 -0700

    Fixed broken arrows

--- combat.missionDebug.js
+++ combat.missionDebug.js
@@ -3,8 +3,8 @@ const { getAllMissions } = require("./combat.missionUtility");
 const { drawArrow } = require("./debug.mapOverlay");
 
 const COLOR_BY_MISSION_TYPE = {
-    [MISSION_TYPES.COLONIZE]: "6BEB2A",
-    [MISSION_TYPES.KILL]: "EE1111",
+    [MISSION_TYPES.COLONIZE]: "#6BEB2A",
+    [MISSION_TYPES.KILL]: "#F53520",
 };
 
 const showMissionTargets = () => {
@@ -14,7 +14,7 @@ const showMissionTargets = () => {
             drawArrow(mission, colony, {
                 color: COLOR_BY_MISSION_TYPE[allMissions[mission].type],
                 lineStyle: "dotted",
-                width: 1.5,
+                width: 1.8,
             });
         }
     }
