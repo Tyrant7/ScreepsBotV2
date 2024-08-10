8017542eb349663066780b8e1ecd0e302eaa851a
Tyrant <tyler.sauve@bag-of-holding.com>
2024-08-10 10:53:08 -0700

    Duo pairing setup

--- creep.duo.js
+++ creep.duo.js
@@ -4,9 +4,22 @@ const { pathSets } = require("./constants");
 
 class DuoManager extends CreepManager {
     createTask(creep, colony) {
+        // Let's pair all unpaired duos, superior with inferior
+        if (!creep.memory.pair) {
+            const pair = colony.combatDuos.find(
+                (d) =>
+                    d.memory.superior !== creep.memory.superior &&
+                    !d.memory.pair
+            );
+            if (pair) {
+                creep.memory.pair = pair.name;
+                pair.memory.pair = creep.name;
+            }
+        }
+
         return new Task({}, "exist", [
             function (creep, colony) {
-                creep.say("Duo " + (creep.memory.superior ? "lead" : "follow"));
+                creep.say("Duo " + creep.memory.pair);
                 return false;
             },
         ]);
